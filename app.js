6296df3d2a68f05ef91bb1a59b4f2df8fff81c87
Sivamani <5maurosivamani@gmail.com>
2022-06-09 17:32:42 +0530

    Level - 4 Salting and Hashing with bcrypt

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const mongoose = require("mongoose")
-const md5 = require("md5")
+const bcrypt = require("bcrypt")
+const saltRounds = 10
+
 
 const app = express()
 
@@ -37,32 +39,39 @@ app.get("/login", (req, res)=>{
 
 // REGISTER A NEW USER 
 app.post("/register", (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    })
 
-    newUser.save((err) => {
-        if(!err) {
-            console.log("Successfully Inserted a new user.");
-            res.render("secrets")
-        }
-    })
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        })
+    
+        newUser.save((err) => {
+            if(!err) {
+                console.log("Successfully Inserted a new user.");
+                res.render("secrets")
+            }
+        })
+    });
+   
 })
 
 // LOGIN EXISTING USER WITH CREDENTIAL
 app.post("/login", (req, res) => {
     const userName = req.body.username
-    const password = md5(req.body.password)
+    const password = req.body.password
 
     User.findOne({email: userName}, (err, foundResult) => {
         if(err) {
             res.send(err)
         } else {
             if(foundResult) {
-                if(foundResult.password === password) {
-                    res.render("secrets")
-                } 
+                // Load hash from password DB.
+                bcrypt.compare(password, foundResult.password, function(err, result) {
+                    if(result == true) {
+                        res.render("secrets")
+                    }
+                });
             } 
         }
     })
